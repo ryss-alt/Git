033366067f46f06a3144f857834a0ac6e598c97b
ryss-alt <ryoyavideos@gmail.com>
2026-03-24 21:54:25 +0900

    Fix Package.swift for Swift Playgrounds 4 using iOSApplication product

--- ChineseLearning.swiftpm/Package.swift
+++ ChineseLearning.swiftpm/Package.swift
@@ -6,6 +6,27 @@ let package = Package(
     platforms: [
         .iOS(.v17)
     ],
+    products: [
+        .iOSApplication(
+            name: "ChineseLearning",
+            targets: ["ChineseLearning"],
+            bundleIdentifier: "com.example.chineselearning",
+            teamIdentifier: "",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: .placeholder(icon: .book),
+            accentColor: .presetColor(.blue),
+            supportedDeviceFamilies: [
+                .pad,
+                .phone
+            ],
+            supportedInterfaceOrientations: [
+                .portrait,
+                .landscapeLeft,
+                .landscapeRight
+            ]
+        )
+    ],
     targets: [
         .executableTarget(
             name: "ChineseLearning",
